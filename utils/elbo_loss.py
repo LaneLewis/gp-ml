4172d924384e9042ae082f64578737555e3f25a5
lanelewis <lrlewis@andrew.cmu.edu>
2023-11-10 15:58:48 -0500

    updated with kl term

--- utils/elbo_loss.py
+++ utils/elbo_loss.py
@@ -1,6 +1,7 @@
 import torch
 
-def approx_elbo_loss(batch_X:torch.Tensor,encoding_function:function,decoding_function:function,R:torch.Tensor,samples:int=100)->torch.Tensor:
+def approx_elbo_loss(batch_X:torch.Tensor,encoding_function:function,decoding_function:function,
+                     R:torch.Tensor,K:torch.tensor,samples:int=100)->torch.Tensor:
     '''
     computes the elbo loss for a batched dataset
     batch_X - tensor of shape [batch_size, timesteps, observation_dims] : The batched dataset to compute the loss over
@@ -18,11 +19,16 @@ def approx_elbo_loss(batch_X:torch.Tensor,encoding_function:function,decoding_fu
     returns individual_elbos - shape [batch_size]: loss for each sample of X in the batch
     '''
     encoding_mus,encoding_Sigmas = encoding_function(batch_X)
+    #computes the ll expectation term
     encoding_dist_samples = sample_encoding_dist(encoding_mus,encoding_Sigmas,samples)
     decoding_manifold_means = decoding_function(encoding_dist_samples)
     log_likelihood_sample_losses = log_likelihood_loss(batch_X,decoding_manifold_means,R)
-    approx_individual_elbos= torch.mean(log_likelihood_sample_losses,dim=1)
-    return approx_individual_elbos
+    approx_individual_log_likelihood= torch.mean(log_likelihood_sample_losses,dim=1)
+    #computes the kl divergence term
+    kl_divergence_term = kl_divergence(encoding_mus,encoding_Sigmas,K)
+    #combines them together to get the approximate elbo per item in the batch
+    individual_elbos = -1*approx_individual_log_likelihood + kl_divergence_term
+    return individual_elbos
 
 def sample_encoding_dist(encoding_mus:torch.Tensor,encoding_Sigmas:torch.Tensor,samples:int)->torch.Tensor:
     '''
@@ -43,6 +49,21 @@ def log_likelihood_loss(batch_X,decoding_manifold_means,R)->torch.tensor:
     batch_X - tensor of shape [batch_size, timesteps, observation_dims]: the batched data
     decoding_manifold_means - tensor of shape [batch_size, samples timesteps, observation_dims]: 
                               manifold mean embedding by passing the random latent samples through the decoder
-    R - tensor of shape 
+    R - tensor of shape [observation_dims,observation_dims]: the 
     returns log_likelihood_losses - tensor of shape [batch_size, samples]
     '''
+    raise NotImplementedError
+
+    return 
+
+def kl_divergence(encoding_mus,encoding_Sigmas,K)->torch.Tensor:
+    '''
+    computes the kl divergence between the encoding distribution and the prior distribution across batches
+    encoding_mus - tensor of shape [batch_size, timesteps, observation_dims]
+    encoding_Sigmas - tensor of shape [batch_size, timesteps, timesteps, observation_dims]
+    K - tensor of shape [timesteps, timesteps]
+
+    returns kl_divergences - tensor of shape [batch_size]
+    '''
+    raise NotImplementedError
+    return 
